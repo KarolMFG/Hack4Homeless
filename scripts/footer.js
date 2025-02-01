99194effb42998e49e0492deb697e1f5443c1899
r128 <111167388+r128w@users.noreply.github.com>
2025-02-01 12:28:14 -0500

    add links to footer
    really nothing to do lmao

--- scripts/footer.js
+++ scripts/footer.js
@@ -6,7 +6,7 @@ var footer = document.createElement('footer')
 footer.innerHTML = 
 `
     <p style="font-family:'Didot';font-size:medium" class="logo">Hack4Homeless</p>
-    <p>Karol Grondelski & Amin Smith</p>
+    <p><span onclick="location.href='https://github.com/karolmfg'">Karol Grondelski</span> &amp; <span onclick="location.href='https://github.com/r128w'">Amin Smith</span></p>
 `
 
 document.body.appendChild(footer)
