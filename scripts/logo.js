3d7210ee296a525105cf5fa459325b50df15a41a
r128 <111167388+r128w@users.noreply.github.com>
2025-02-01 10:33:33 -0500

    Add files via upload

---
+++ scripts/logo.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const es = document.getElementsByClassName("logo")// elements
+    for(var i = 0; i < es.length; i++){
+        es[i].addEventListener('click', function(){location.href="https://karolmfg.github.io/Hack4Homeless/index.html"})
+    }
+
+})
