7c9018aab9de19322a9b61ecf4e6ad4844d533cd
r128 <111167388+r128w@users.noreply.github.com>
2025-02-01 10:58:17 -0500

    remove event in logo.js
    since it is only loaded once domcontentloaded is triggered
    
    also added a little note

--- scripts/logo.js
+++ scripts/logo.js
@@ -1,8 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(){
 
-    const es = document.getElementsByClassName("logo")// elements
-    for(var i = 0; i < es.length; i++){
-        es[i].addEventListener('click', function(){location.href="https://karolmfg.github.io/Hack4Homeless/index.html"})
-    }
+// gives all elements of class 'logo' hyperlinking function to main page
+// intended to be used on the h1s in the header of each subpage
 
-})
+// doesnt wait for domcontentloaded bc it is only loaded by script.js once that event has triggered anyway
+
+const es = document.getElementsByClassName("logo")// elements
+for(var i = 0; i < es.length; i++){
+    es[i].addEventListener('click', function(){location.href="https://karolmfg.github.io/Hack4Homeless/index.html"})
+}
